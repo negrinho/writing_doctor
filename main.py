a458b4a71f66e1dfcbcf824f87017425405ed65a
Renato Negrinho <negrinho@cs.cmu.edu>
2019-12-17 15:42:50 -0500

    Minor updates

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ nltk.download('averaged_perceptron_tagger', quiet=True)
 def split_sentence_into_word_tokens(sentence):
     return nltk.word_tokenize(sentence)
 
-def tokenize_string_into_words(s):
 
+def tokenize_string_into_words(s):
     return nltk.word_tokenize(s)
 
 
 def sort_dict_items(d, by_key=True, decreasing=False):
     key_fn = (lambda x: x[0]) if by_key else (lambda x: x[1])
     return sorted(d.items(), key=key_fn, reverse=decreasing)
+
 
 def topk(key2cnt, k):
     return sort_dict_items(key2cnt, by_key=False, decreasing=True)[:k]
@@ -54,12 +55,9 @@ def write_jsonfile(d, filepath, sort_keys=False, compactify=False):
         json.dump(d, f, indent=indent, sort_keys=sort_keys)
 
 
-
-
 def begin_end_indices_for_command_parens(s, cmd):
-    idx = s.index(
-        "\\" + cmd
-    )  # NOTE: might not work correctly if prefixes are shared by multiple commands
+    # NOTE: might not work correctly if prefixes are shared by multiple commands
+    idx = s.index("\\" + cmd)
     assert idx is not None
     depth = 0
     for i, ch in enumerate(s[idx:]):
@@ -117,6 +115,7 @@ def parse_caption(lines, idx):
 def read_latexfile(filepath,
                    ignore_comments=True,
                    lowercase=False,
+                   has_begin_end_document=True,
                    tokenize_into_words=True,
                    tokenize_inline_math=True,
                    tokenize_commands_lst=None):
@@ -125,9 +124,10 @@ def read_latexfile(filepath,
         lines = [s for s in lines if not s.startswith("%")]
 
     # remove environments
-    start_idx = lines.index("\\begin{document}")
-    end_idx = lines.index("\\end{document}")
-    lines = lines[start_idx + 1:end_idx]
+    if has_begin_end_document:
+        start_idx = lines.index("\\begin{document}")
+        end_idx = lines.index("\\end{document}")
+        lines = lines[start_idx + 1:end_idx]
 
     out_lines = []
     inside_env_depth = 0
@@ -163,10 +163,10 @@ def read_latexfile(filepath,
             if s != '':
                 line.append(s)
     lines = out_lines
-    out_lines = []
 
     # tokenize math (TODO: make it work for display math; this does not check if $ are escaped or not.)
     if tokenize_inline_math:
+        out_lines = []
         for s in lines:
             out_s = s
             while "$" in out_s:
@@ -174,10 +174,10 @@ def read_latexfile(filepath,
                 end_idx = start_idx + out_s[start_idx + 1:].index("$")
                 out_s = out_s[:start_idx] + " -MATH- " + out_s[end_idx + 2:]
             out_lines.append(out_s)
+        lines = out_lines
 
-    lines = out_lines
-    out_lines = []
     if tokenize_commands_lst is not None:
+        out_lines = []
         for s in lines:
             out_s = s
             any_match = True
@@ -193,10 +193,11 @@ def read_latexfile(filepath,
                         out_s = left_s + " -" + cmd.upper() + "- " + right_s
                         any_match = True
             out_lines.append(out_s)
+        lines = out_lines
 
     # tokenize into sentences and words
     sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-    ps = [sent_detector.sentences_from_text(s) for s in out_lines]
+    ps = [sent_detector.sentences_from_text(s) for s in lines]
     if tokenize_into_words:
         ps = [[nltk.word_tokenize(s) for s in p] for p in ps]
     if lowercase:
@@ -204,8 +205,10 @@ def read_latexfile(filepath,
     return ps
 
 
+# TODO: add a file path
 def read_latexfile_outline(filepath, include_paragraphs=True):
     lines = read_textfile(filepath, strip=True)
+    out_lines = []
     for s in lines:
         match = False
         prefix = ""
@@ -226,7 +229,39 @@ def read_latexfile_outline(filepath, include_paragraphs=True):
             pass
 
         if match:
-            print prefix + s[s.index("{") + 1:s.index("}")]
+            s = prefix + s[s.index("{") + 1:s.index("}")]
+            out_lines.append(s)
+    return out_lines
+
+
+# TODO: add an ignore comments option. for now, always ignores.
+def read_markdown(filepath, lowercase=False, tokenize_into_words=True):
+    lines = read_textfile(filepath, strip=True)
+
+    # join lines that are not separated by an empty line.
+    out_lines = []
+    line = []
+    is_inside_code = False
+    for s in lines:
+        if s == '' and len(line) > 0 and not is_inside_code:
+            out_s = ' '.join(line)
+            out_lines.append(out_s)
+            line = []
+        else:
+            if s != '' and not is_inside_code:
+                line.append(s)
+
+        if s.startswith("```"):
+            is_inside_code = not is_inside_code
+
+    # tokenize into sentences and words
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+    ps = [sent_detector.sentences_from_text(s) for s in out_lines]
+    if tokenize_into_words:
+        ps = [[nltk.word_tokenize(s) for s in p] for p in ps]
+    if lowercase:
+        ps = [[[w.lower() for w in s] for s in p] for p in ps]
+    return ps
 
 
 def get_todos(ps):
@@ -249,13 +284,43 @@ def get_pos_tags_for_sentence(s):
 def apply_to_paragraphs(ps, sentence_fn):
     return [[sentence_fn(s) for s in p] for p in ps]
 
+def get_adjective_counts(ps):
+    w2c = {}
+    for p in ps:
+        for s in p:
+            tags = get_pos_tags_for_sentence(s)
+            for i, t in enumerate(tags):
+                if t.startswith("JJ"):
+                    w = s[i]
+                    if w not in w2c:
+                        w2c[w] = 0
+                    w2c[w] += 1
+    return w2c
 
-def show_sentences(ps, str_fn):
+def get_adjective_counts(s):
+    w2c = {}
+    for p in ps:
+        for s in p:
+            tags = get_pos_tags_for_sentence(s)
+            for i, t in enumerate(tags):
+                if t.startswith("RB"):
+                    w = s[i]
+                    if w not in w2c:
+                        w2c[w] = 0
+                    w2c[w] += 1
+    return w2c
+
+def show_sentences(ps, str_fn, print_to_terminal=True):
+    out_lines = []
     for i, p in enumerate(ps):
         for s in p:
             out = str_fn(s)
             if len(out) > 0:
-                print "%d => %s" % (i, out)
+                s = "%d => %s" % (i, out)
+                out_lines.append(s)
+                if print_to_terminal:
+                    print s
+    return out_lines
 
 
 def bad_words_fn(s):
@@ -321,18 +386,19 @@ def get_frequent_long_words(ps, len_thres, count_thres, top_k):
 
 # # NOTE: the API design needs to be revised.
 # # TODO: add something that keep the paragraph structure and something that does not.
-def sort_sentences_by_word_length(ps, filepath=None):
+def sort_sentences_by_word_length(ps, filepath=None, print_to_terminal=True):
     lst = sorted([s for p in ps for s in p], key=lambda s: len(s), reverse=True)
     lines = ["  >>> " + " ".join(s) for s in lst]
 
     if filepath is not None:
         write_textfile(filepath, lines)
-    else:
+    if print_to_terminal:
         for s in lines:
             print s
+    return lines
 
 
-def sort_sentences_by_char_length(ps, filepath=None):
+def sort_sentences_by_char_length(ps, filepath=None, print_to_terminal=True):
     lst = sorted([s for p in ps for s in p],
                  key=lambda s: sum([len(w) for w in s]) + len(s) - 1,
                  reverse=True)
@@ -340,14 +406,32 @@ def sort_sentences_by_char_length(ps, filepath=None):
 
     if filepath is not None:
         write_textfile(filepath, lines)
-    else:
+    if print_to_terminal:
         for s in lines:
             print s
+    return lines
+
+
+def show_paragraph_summary(ps, filepath=None, print_to_terminal=True):
+    lines = []
+    for p in ps:
+        out_s = "|>>> " + " ".join(p[0])
+        lines.append(out_s)
+    # output to a file.
+    if filepath is not None:
+        write_textfile(filepath, lines)
+    if print_to_terminal:
+        for s in lines:
+            print s
+    return lines
 
 
 # TODO: improve formatting overall.
 ### TODO: have a way of doing filepaths outputs. just add the arguments.
-def show_paragraph_sentence_word_summary(ps, num_words, filepath=None):
+def show_paragraph_sentence_word_summary(ps,
+                                         num_words,
+                                         filepath=None,
+                                         print_to_terminal=True):
     lines = []
     for p in ps:
         out_s = "|>>> " + " ".join(p[0])
@@ -359,12 +443,16 @@ def show_paragraph_sentence_word_summary(ps, num_words, filepath=None):
     # output to a file.
     if filepath is not None:
         write_textfile(filepath, lines)
-    else:
+    if print_to_terminal:
         for s in lines:
             print s
+    return lines
 
 
-def show_paragraph_sentence_char_summary(ps, num_chars, filepath=None):
+def show_paragraph_sentence_char_summary(ps,
+                                         num_chars,
+                                         filepath=None,
+                                         print_to_terminal=True):
     lines = []
     for p in ps:
         out_s = "|>>> " + " ".join(p[0])
@@ -376,12 +464,16 @@ def show_paragraph_sentence_char_summary(ps, num_chars, filepath=None):
     # output to a file.
     if filepath is not None:
         write_textfile(filepath, lines)
-    else:
+    if print_to_terminal:
         for s in lines:
             print s
+    return lines
 
 
-def show_sentence_word_summary(ps, num_words, filepath=None):
+def show_sentence_word_summary(ps,
+                               num_words,
+                               filepath=None,
+                               print_to_terminal=True):
     lines = []
     for p in ps:
         for s in p:
@@ -391,12 +483,16 @@ def show_sentence_word_summary(ps, num_words, filepath=None):
     # output to a file.
     if filepath is not None:
         write_textfile(filepath, lines)
-    else:
+    if print_to_terminal:
         for s in lines:
             print s
+    return lines
 
 
-def show_sentence_char_summary(ps, num_chars, filepath=None):
+def show_sentence_char_summary(ps,
+                               num_chars,
+                               filepath=None,
+                               print_to_terminal=True):
     lines = []
     for p in ps:
         for s in p:
@@ -406,12 +502,13 @@ def show_sentence_char_summary(ps, num_chars, filepath=None):
     # output to a file.
     if filepath is not None:
         write_textfile(filepath, lines)
-    else:
+    if print_to_terminal:
         for s in lines:
             print s
+    return lines
 
 
-def show_sentences_with_words(ps, words, filepath=None):
+def show_sentences_with_words(ps, words, filepath=None, print_to_terminal=True):
     lines = []
     for p in ps:
         for s in p:
@@ -421,35 +518,10 @@ def show_sentences_with_words(ps, words, filepath=None):
                     lines.append(out_s)
     if filepath is not None:
         write_textfile(filepath, lines)
-    else:
+    if print_to_terminal:
         for s in lines:
             print s
-
-
-# TODO: add an ignore comments option.
-def read_markdown(filepath, lowercase=False, tokenize_into_words=True):
-    lines = read_textfile(filepath, strip=True)
-
-    # join lines that are not separated by an empty line.
-    out_lines = []
-    line = []
-    for s in lines:
-        if s == '' and len(line) > 0:
-            out_s = ' '.join(line)
-            out_lines.append(out_s)
-            line = []
-        else:
-            if s != '':
-                line.append(s)
-
-    # tokenize into sentences and words
-    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-    ps = [sent_detector.sentences_from_text(s) for s in out_lines]
-    if tokenize_into_words:
-        ps = [[nltk.word_tokenize(s) for s in p] for p in ps]
-    if lowercase:
-        ps = [[[w.lower() for w in s] for s in p] for p in ps]
-    return ps
+    return lines
 
 
 ds = read_jsonfile("map.json")
@@ -461,16 +533,63 @@ for d in ds:
 
 n_longest = max([len(x) for x in words])
 
-ps_md = read_markdown(
-    "/Users/negrinho/Desktop/r2ai/_posts/2019-05-26-server-workflow.md"
-)
-show_sentences(ps_md, bad_words_fn)
-# show_sentences(ps_md, adjectives_fn)
-show_paragraph_sentence_word_summary(ps_md, 16, filepath="summary.txt")
+# ps = read_latexfile(
+#     '/Users/negrinho/Desktop/5cdeeeb63c25b9478af185b2/deep_architect.tex',
+#     lowercase=True, tokenize_inline_math=True,
+#     tokenize_commands_lst=["ref", "cite"])
 
-from pprint import pprint
-# pprint(get_frequent_long_words(ps_md, 8, 0, 128))
-# show_sentences(ps_md, repeats_fn)
+# TODO: allow this to deal wtih folders more neatly.
+
+# "/Users/negrinho/Desktop/lourenco/Thesis_Introduction-pedro.tex"
+# "/Users/negrinho/Desktop/lourenco/Thesis_Background_ObjectDetection.tex"
+# filepath = "/Users/negrinho/Desktop/r2ai/_posts/2019-07-26-introducing-deep-architect.md"
+# filepath = "/Users/negrinho/Desktop/r2ai/_posts/2019-12-01-on-writing.md"
+# ps = read_markdown(filepath, lowercase=True)
+
+
+# filepath = "/Users/negrinho/Desktop/neurips2019_deep_architect/deep_architect.tex"
+# filepath = "/Users/negrinho/Desktop/writing_linting/object_detection.tex"
+filepath = "/Users/negrinho/Desktop/beam_learn/papers/5cebae758c00615e225a1827/naaclhlt2019.tex"
+ps = read_latexfile(filepath, lowercase=True, has_begin_end_document=True, ignore_comments=True)
+
+out_lines = []
+out_lines.extend(["**************** Section outline ****************", ""])
+lst = read_latexfile_outline(filepath, include_paragraphs=True)
+out_lines.extend(lst)
+out_lines.append("")
+out_lines.extend(
+    ["**************** Suggested substitutions ****************", ""])
+lst = show_sentences(ps, bad_words_fn, print_to_terminal=False)
+out_lines.extend(lst)
+out_lines.append("")
+out_lines.extend(["**************** Paragraph summary ****************", ""])
+lst = show_paragraph_summary(ps, print_to_terminal=False)
+out_lines.extend(lst)
+out_lines.append("")
+# show_sentences(ps, adjectives_fn)
+out_lines.extend(["**************** Summary ****************", ""])
+lst = show_paragraph_sentence_word_summary(ps, 16, print_to_terminal=False)
+out_lines.extend(lst)
+out_lines.append("")
+out_lines.extend(["**************** Adjectives ****************", ""])
+lst = show_sentences(ps, adjectives_fn, print_to_terminal=False)
+out_lines.extend(lst)
+out_lines.append("")
+out_lines.extend(["**************** Repeats ****************", ""])
+lst =  show_sentences(ps, repeats_fn, print_to_terminal=False)
+out_lines.extend(lst)
+out_lines.append("")
+write_textfile("summary.txt", out_lines)
+
+
+
+# read_latexfile_outline(filepath)
+
+#### TODO: make some of these easier to do.
+# NOTE: there are some quadratic operations. I think that they have to share a lot of words at least.
+# NOTE: usually a fairly small number of sentences.
+
+# pprint(get_frequent_long_words(ps, 8, 0, 128))
 
 
 # show_sentences_with_words(ps, ["module", "modules"], "module_summary.txt")
@@ -518,7 +637,6 @@ from pprint import pprint
 
 # get this information from some online datasets about synonyms and what not.
 
-
 # TODO: to collect data, we can use something like a continuous
 # running git that will capture differences between sentences.
 # if we have a bunch of snapshots, I think that it
@@ -549,3 +667,31 @@ from pprint import pprint
 # warn, strict.
 
 # python writing_linter.py --json_config xxx --levels='["a", "b", "c"]'
+
+# TODO: change the way that it is shown there.
+
+# TODO: introduce basic stuff with adjectives.
+# TODO: makes more sense to write things in the file at the end.
+# NOTE: makes it easier to use too.
+
+# NOTE: some are tokenized, some are not.
+# distinguish this. tk_ps, vs ps, lines, tk_lines, tk_out_lines.
+# ps are always tokenized, I think.
+
+# NOTE: I would say, some of them are responsible to return things in a format that is directly printable.
+
+# TODO: for this, make it easy to generate these reduced documents.
+
+# add words that end in ed,
+
+# TODO: needs to be kept into different models. this is important.
+#
+
+# there are a few of these that truly require traversal to work.
+# TODO: this is needed to get it to wokr
+
+# words ending in ition are also interesting.
+
+# TODO: have a way of extracting comments that match a certain thing.
+
+# TODO: add the part of the model that repeats things.
